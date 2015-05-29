34fcaede768af94c856b036da05c46f03a60da48
ralf <kingarthas37@hotmail.com>
2015-05-30 00:36:04 +0800

    git

--- cloud/main.js
+++ cloud/main.js
@@ -3,3 +3,7 @@
 AV.Cloud.define("hello", function(request, response) {
   response.success("Hello world!");
 });
+
+AV.Cloud.define("hello1", function(request, response) {
+  response.success("Hello world!1");
+});
